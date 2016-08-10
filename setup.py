60191a352bf79a070738427cbf653e8e2cbe68bd
Antonis Kalipetis <akalipetis@gmail.com>
2016-08-10 16:08:54 +0300

    Update installation instructions
    Include correct pip installations and Windows workaround the pip-installed docker-compose

--- setup.py
+++ setup.py
@@ -7,12 +7,15 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click',
-        'clint',
-        'pyyaml',
-        'tabulate',
-        'requests',
+        'Click >= 6.0',
+        'PyYAML >= 3.11',
+        'tabulate >= 0.7.5',
+        'requests >= 2.6.1',
     ],
+    extras_require={
+        'security': ['requests[security] >= 2.6.1'],
+        'compose': ['requests >= 2.6.1, < 2.8', 'docker-compose >= 1.7.0'],
+    },
     entry_points='''
     [console_scripts]
     stolos=stolos.cli:cli
