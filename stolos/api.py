d4bae82ceaf2d41eced0b57580a764c5a537d970
Antonis Kalipetis <akalipetis@gmail.com>
2016-08-10 14:58:13 +0300

    Add error handling for the CLI
    If a response is not successful, the error is retrieved and presented to the user.

--- stolos/api.py
+++ stolos/api.py
@@ -2,6 +2,7 @@
 API client for use in the Stolos CLI.
 """
 import os.path
+from functools import wraps
 
 import click
 import requests
@@ -9,24 +10,48 @@ import requests
 from stolos import exceptions
 
 
+def handle_api_errors(func):
+    """
+    Decorator for handling API errors. Catches `requests.exceptions.HTTPError`
+    and throws the appropriate `exceptions.*` error.
+    """
+    @wraps(func)
+    def func_wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except requests.exceptions.HTTPError as err:
+            if err.response.status_code / 100 == 5:
+                raise exceptions.ServerError(err.response.text)
+            elif err.response.status_code in [401, 403]:
+                raise exceptions.Unauthorized(err.response.json())
+            elif err.response.status_code == 400:
+                raise exceptions.BadRequest(err.response.json())
+            elif err.response.status_code == 404:
+                raise exceptions.ResourceDoesNotExist(err.response.json())
+            else:
+                raise exceptions.UnknownError(
+                    err.response.status_code, err.response.text)
+        except requests.exceptions.ConnectionError as err:
+            raise exceptions.NoInternetException()
+        except requests.exceptions.Timeout as err:
+            raise exceptions.Timeout()
+    return func_wrapper
+
+
+@handle_api_errors
 def authenticate(stolos_url, username, password):
     """
     Authenticate the user to the given Stolos server, using the given
     credentials. Returns the authentication token.
     """
     url = os.path.join(stolos_url, 'api/a0.1/auth/login/')
-    try:
-        resp = requests.post(url,
-                             json={'username': username, 'password': password})
-        resp.raise_for_status()
-        return resp.json()
-    except requests.exceptions.HTTPError as err:
-        raise click.UsageError(
-            'Wrong authentication credentials')
-    except requests.exceptions.ConnectionError as err:
-        raise exceptions.NoInternetException()
+    resp = requests.post(
+        url, json={'username': username, 'password': password})
+    resp.raise_for_status()
+    return resp.json()
 
 
+@handle_api_errors
 def projects_list(credentials):
     """
     List the projects of the currently logged in user.
@@ -38,6 +63,7 @@ def projects_list(credentials):
     return resp.json()
 
 
+@handle_api_errors
 def projects_create(credentials, stack, public_url):
     """
     Create a new project, using the given stack and public URL.
@@ -57,6 +83,7 @@ def projects_create(credentials, stack, public_url):
     return resp.json()
 
 
+@handle_api_errors
 def projects_retrieve(credentials, project_uuid):
     """
     Retrieve the project with the given UUID.
@@ -68,6 +95,7 @@ def projects_retrieve(credentials, project_uuid):
     return resp.json()
 
 
+@handle_api_errors
 def projects_remove(credentials, project_uuid):
     """
     Remove the proejct with the given UUID.
