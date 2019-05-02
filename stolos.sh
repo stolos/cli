f2ab3356931a84e8f09cc691203d194b513814bb
Paris Kasidiaris <paris@sourcelair.com>
2019-05-02 11:27:47 +0300

    Remove obsolete files.

--- stolos.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-if [ -z $STOLOS_PROJECT_ID ]; then
-    echo 'Please define STOLOS_PROJECT_ID env variable';
-    exit 1;
-fi
-
-exec docker-compose -p ${STOLOS_PROJECT_ID} "$@"
--- sync.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-if [ -z $STOLOS_PROJECT_ID ]; then
-    echo 'Please define STOLOS_PROJECT_ID env variable';
-    exit 1;
-fi
-
-unison -repeat 2 $(pwd) "ssh://stolos-unison//mnt/stolos/${STOLOS_PROJECT_ID}"
